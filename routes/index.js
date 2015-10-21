f59d038892d71ae1c2b20e0e6af284e929b2b662
U-WW001\z002ew6p <z002ew6p@MD1EEV4C.ww001.siemens.net>
2015-10-21 14:34:48 +0200

    synchronous port scanning added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var evilscan = require('evilscan');
 var portscanner = require('portscanner');
 var async=require('async');
-var host = '192.168.178.49/24';
+var host = '127.0.0.1';
 // starting from port number
 var start = 80;
 // to port number
@@ -17,7 +17,7 @@ router.get('/', function(req, res, next) {
 
 var range=getRange(host);
 async.each(range,function(ip,callback){
-  scan(ip,function(){
+  scanSynchronous(ip,function(){
     console.log(ip+ " finished");
     callback();
   });
@@ -26,7 +26,6 @@ async.each(range,function(ip,callback){
   console.log('all finished');
 
 });
-
 
 console.log('scanner finished');
  // res.render('index', { title: 'Express' });
@@ -44,41 +43,55 @@ function getRange(ipSubnet)
 
   return range;
 }
-function scan(ip,callback)
+
+function getListFromRange(start,end)
 {
-  var net = require('net');
-
-        console.log("starting scanning ip:"+ip);
-        // the port scanning loop
-        var port = start;
-        while (port <= end) {
-          (function(port) {
-            // console.log('CHECK: ' + port);
-            var s = new net.Socket();
-
-            s.setTimeout(timeout, function() { s.destroy(); });
-            s.connect(port, ip, function() {
-              console.log('OPEN: ' + ip + ":" + port);
-              callback();
-            });
-
-            // if any data is written to the client on connection, show it
-            s.on('data', function(data) {
-              console.log(port +': '+ data);
-              s.destroy();
-              callback();
-            });
-
-            s.on('error', function(e) {
-
-              console.error('closed port:'+ip + ":" +port);
-              s.destroy();
-              callback();
-            });
-          })(port);
-
-          port++;
-        }
+  var list = new Array();
+  if (start>end)
+  {
+    return null;
+  }
+  while (start<=end)
+  {
+    list.push(start);
+    start++;
+  }
 }
+
+function scanSynchronous(ip,callback)
+{
+  var portList=getListFromRange(start,end);
+  var asynchPortScan=require('async');
+
+  async.each(portList,function(port,callback){
+    var s = new net.Socket();
+    s.setTimeout(timeout, function() { s.destroy(); });
+
+    s.connect(port, ip, function() {
+      console.log('OPEN: ' + ip + ":" + port);
+      callback();
+    });
+
+    // if any data is written to the client on connection, show it
+    s.on('data', function(data) {
+      console.log(port +': '+ data);
+      s.destroy();
+      callback();
+    });
+
+
+    s.on('error', function(e) {
+
+      console.error('closed port:'+ip + ":" +port);
+      s.destroy();
+      callback();
+    });
+
+    callback();
+
+  });
+
+}
+
 
 module.exports = router;
